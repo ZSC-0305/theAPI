e03b23353b71b22d13be7390c8990596315f32d8
peso <1711577439@qq.com>
2025-04-28 22:28:45 +0800

    fix: final serverless config

--- api/server.js
+++ api/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 const cors = require('cors');
 
 
 app.use(cors());
+app.use(express.json());
 app.use((req,res,next)=>{
   res.success = (data)=>{
     res.json({
@@ -69,9 +70,9 @@ app.get('/content', (req, res) => {
   res.success(partners);
 });
 
-// 启动服务器
-app.listen(PORT, () => {
-  console.log(`服务已启动，访问地址：http://localhost:${PORT}`);
-});
+// // 启动服务器
+// app.listen(PORT, () => {
+//   console.log(`服务已启动，访问地址：http://localhost:${PORT}`);
+// });
 
 module.exports = app;
